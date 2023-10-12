8d5edba2bd882dbf6093bf82892d9f04d0ab4c89
TonyMM55 <tonymmoran@gmail.com>
2023-10-12 12:38:42 +0100

    added about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}
+        >Introduction</p>
+        <h2 className={styles.sectionHeadText}
+        >Overview</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+         I'm a skilled software developer with experience in JavaScript, and libraries like React, NodeJS and ThreeJS. I'm a quick learner and collaborate closely with clients to create efficient, scalable and user friendly applications that solve real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+    </>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,16 +9,16 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={1.5} groundColor='black' />
+      <hemisphereLight intensity={5.5} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={5}
+        intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={1.5} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
